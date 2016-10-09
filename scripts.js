0331a836c34f6b78f32acfba5f48cd1d30c508f5
jkbits1 <jkbits1@users.noreply.github.com>
2016-10-09 22:26:29 +0100

    tidying

--- scripts.js
+++ scripts.js
@@ -1,12 +1,5 @@
 // MyScript.js
 
-
-
-	
-
-
-
-
 	//timers
 	
 
@@ -17,13 +10,48 @@
 	// 	clearInterval(timerId);
 	// }
 
+// MyScript.js
+
+pageFn();
+
+function pageFn() {
+	var switchFrame = document.getElementById("flicker");
+  var playerY = 0; //where should the top be at the beginning?
+ 
+  function movePlayer() {
+		switchFrame.style.display="block";
+
+		setTimeout(function(){switchFrame.style.display="none";}, 100)
+
+	// what is the top at the moment we should loop back?
+		playerY = (playerY > -4000) ? playerY - 480 : 0;
+		// console.log(playerY);
+
+		$('#player').css("top", playerY);
+	}
+        
+  setInterval(movePlayer, 1000);
+}
 
 
+function heleneFn() {
 
+	var playerY = 0; //where should the top be at the beginning?
 
+	function movePlayer() {
+		if (playerY < 625) // what is the top at the moment we should loop back?
+		{
+			playerY = playerY + 3;
+		}
+		else {
+			playerY = 0;
+		}
+		console.log(playerY);
 
+		$('#player').css("top", playerY);
+	}
 
-
-
+	setInterval(movePlayer, 0.7);
+}
 
 
